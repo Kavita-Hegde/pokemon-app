74af98affdea4c3fb80d6d36e34906d6c05196e2
Kavita-Hegde <kavitashegde4@gmail.com>
2020-02-12 21:41:52 +0530

    Updates

---
+++ precache-manifest.b76b7ce8c149129e5e4bc7baa43f5b55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dff51efab691cb2f7fa0b8a58c72c8b",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "e795a8b2c2abb6cc36bf",
+    "url": "/pokemon-app/static/css/main.1876f6e4.chunk.css"
+  },
+  {
+    "revision": "f3190c72aa1378639fb7",
+    "url": "/pokemon-app/static/js/2.ed92eb9c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pokemon-app/static/js/2.ed92eb9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e795a8b2c2abb6cc36bf",
+    "url": "/pokemon-app/static/js/main.d1e09c13.chunk.js"
+  },
+  {
+    "revision": "ba6b6484f7eeaa88e8ab",
+    "url": "/pokemon-app/static/js/runtime-main.f7e950a9.js"
+  }
+]);
